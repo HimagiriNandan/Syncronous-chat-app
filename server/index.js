123da38c4591ffa5a360c7de0b02f9c5a17bd172
Himagiri Nandan <himagirinandantuduru@gmail.com>
2024-10-19 22:05:46 +0530

    credentials et to true in cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const dbUrl = process.env.DATABASE_URL;
 const corsOptions = {
   origin: "https://syncronous-chat-app-vgjz.vercel.app",
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-  credentials: false
+  credentials: true
 }
 
 app.use(cors(corsOptions));
