291682b37081c4bcdebab32dcd4d0a0980aa00df
Himagiri Nandan <himagirinandantuduru@gmail.com>
2024-10-19 21:57:27 +0530

    managed cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3001;
 const dbUrl = process.env.DATABASE_URL;
 
 const corsOptions = {
-  origin: ["https://syncronous-chat-app-vgjz.vercel.app", "https://localhost:5173"],
+  origin: "https://syncronous-chat-app-vgjz.vercel.app",
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   credentials: true
 }
